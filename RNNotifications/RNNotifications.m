2d16bc4b10691bedc392796ff8876f784ba9d816
Ryan Eberhardt <rebs@whatsapp.com>
2017-08-10 11:23:21 -0700

    Use UserNotifications to create local notifications
    a15e038a adds support for enumerating/dismissing delivered notifications
    by ID, but UserNotifications notifications and UILocalNotification
    notifications have a different set of IDs (a notification created using
    UILocalNotification will have an ID that does not match the
    notification's ID in the UserNotifications world and hence cannot be
    used to dismiss that notification). This commit creates the
    notifications using UserNotifications if the user is on iOS 10.

--- RNNotifications/RNNotifications.m
+++ RNNotifications/RNNotifications.m
@@ -17,6 +17,8 @@
 #import "RNNotificationsBridgeQueue.h"
 #import <UserNotifications/UserNotifications.h>
 
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+
 NSString* const RNNotificationCreateAction = @"CREATE";
 NSString* const RNNotificationClearAction = @"CLEAR";
 
@@ -105,6 +107,38 @@ RCT_ENUM_CONVERTER(UIUserNotificationActionBehavior, (@{
     notification.category = [RCTConvert NSString:details[@"category"]];
 
     return notification;
+}
+@end
+
+@implementation RCTConvert (UNNotificationRequest)
++ (UNNotificationRequest *)UNNotificationRequest:(id)json withId:(NSString*)notificationId
+{
+    NSDictionary<NSString *, id> *details = [self NSDictionary:json];
+
+    UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+    content.body = [RCTConvert NSString:details[@"alertBody"]];
+    content.title = [RCTConvert NSString:details[@"alertTitle"]];
+    content.sound = [RCTConvert NSString:details[@"soundName"]]
+        ? [UNNotificationSound soundNamed:[RCTConvert NSString:details[@"soundName"]]]
+        : [UNNotificationSound defaultSound];
+    content.userInfo = [RCTConvert NSDictionary:details[@"userInfo"]] ?: @{};
+    content.categoryIdentifier = [RCTConvert NSString:details[@"category"]];
+
+    NSDate *triggerDate = [RCTConvert NSDate:details[@"fireDate"]];
+    UNCalendarNotificationTrigger *trigger = nil;
+    if (triggerDate != nil) {
+        NSDateComponents *triggerDateComponents = [[NSCalendar currentCalendar]
+                                                   components:NSCalendarUnitYear +
+                                                   NSCalendarUnitMonth + NSCalendarUnitDay +
+                                                   NSCalendarUnitHour + NSCalendarUnitMinute +
+                                                   NSCalendarUnitSecond + NSCalendarUnitTimeZone
+                                                   fromDate:triggerDate];
+        trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:triggerDateComponents
+                                                                           repeats:NO];
+    }
+
+    return [UNNotificationRequest requestWithIdentifier:notificationId
+                                                content:content trigger:trigger];
 }
 @end
 
@@ -545,25 +579,35 @@ RCT_EXPORT_METHOD(consumeBackgroundQueue)
 
 RCT_EXPORT_METHOD(localNotification:(NSDictionary *)notification withId:(NSString *)notificationId)
 {
-    UILocalNotification* localNotification = [RCTConvert UILocalNotification:notification];
-    NSMutableArray* userInfo = localNotification.userInfo.mutableCopy;
-    [userInfo setValue:notificationId forKey:@"__id"];
-    localNotification.userInfo = userInfo;
-
-    if ([notification objectForKey:@"fireDate"] != nil) {
-        [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10")) {
+        UNNotificationRequest* localNotification = [RCTConvert UNNotificationRequest:notification withId:notificationId];
+        [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:localNotification withCompletionHandler:nil];
     } else {
-        [[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
+        UILocalNotification* localNotification = [RCTConvert UILocalNotification:notification];
+        NSMutableArray* userInfo = localNotification.userInfo.mutableCopy;
+        [userInfo setValue:notificationId forKey:@"__id"];
+        localNotification.userInfo = userInfo;
+
+        if ([notification objectForKey:@"fireDate"] != nil) {
+            [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+        } else {
+            [[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
+        }
     }
 }
 
 RCT_EXPORT_METHOD(cancelLocalNotification:(NSString *)notificationId)
 {
-    for (UILocalNotification* notification in [UIApplication sharedApplication].scheduledLocalNotifications) {
-        NSDictionary* notificationInfo = notification.userInfo;
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10")) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        [center removePendingNotificationRequestsWithIdentifiers:@[notificationId]];
+    } else {
+        for (UILocalNotification* notification in [UIApplication sharedApplication].scheduledLocalNotifications) {
+            NSDictionary* notificationInfo = notification.userInfo;
 
-        if ([[notificationInfo objectForKey:@"__id"] isEqualToString:notificationId]) {
-            [[UIApplication sharedApplication] cancelLocalNotification:notification];
+            if ([[notificationInfo objectForKey:@"__id"] isEqualToString:notificationId]) {
+                [[UIApplication sharedApplication] cancelLocalNotification:notification];
+            }
         }
     }
 }
